73eec146531e001b86abd7ad3b57edf7ccc9f952
Taanveer22 <taanveer469@gmail.com>
2025-12-27 02:59:59 +0600

    alhamdulliah navbar complete and json data added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <section className="bg-base-300">
+        <div className="py-8 px-4 mx-auto min-h-screen lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-600 ">
+              404
+            </h1>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
+              Something's missing.
+            </p>
+            <p className="mb-4 text-lg font-light text-gray-800 ">
+              Sorry, we can't find that page. You'll find lots to explore on the
+              home page.
+            </p>
+            <Link
+              to="/"
+              className="inline-flex text-black bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 btn btn-success"
+            >
+              Back to Homepage
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default NotFound;
